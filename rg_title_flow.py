fba7d85f96abe77220c41870f6dd99222b7781dd
Ariciv22 <wiktor2202011@gmail.com>
2026-07-18 22:19:16 +0200

    Zachowaj tlo menu podczas konfiguracji nowej gry

---
+++ rg_title_flow.py
@@ -0,0 +1,29 @@
+import sys
+
+import pygame
+
+import rg_screens as _screens
+
+
+def _sync_layout(screen):
+    width, height = screen.get_size()
+    _screens.SCREEN_WIDTH = width
+    _screens.SCREEN_HEIGHT = height
+    return width, height
+
+
+def _draw_title_background(screen, dim_alpha):
+    width, height = _sync_layout(screen)
+    background = _screens._load_menu_background((width, height))
+    if background:
+        screen.blit(background, (0, 0))
+    else:
+        screen.fill(_screens.BG)
+
+    if dim_alpha > 0:
+        shade = pygame.Surface((width, height), pygame.SRCALPHA)
+        shade.fill((0, 0, 0, dim_alpha))
+        screen.blit(shade, (0, 0))
+
+
+def _draw_caption(screen, font, text, position="center")
