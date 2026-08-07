a03ed8e0b13bb82185b9585ebb2d036cda9ad9d7
Ariciv22 <wiktor2202011@gmail.com>
2026-08-07 11:24:58 +0200

    Rejestruj Wydarzenia Swiata

--- rg_content/__init__.py
+++ rg_content/__init__.py
@@ -2,7 +2,16 @@
 
 from rg_content.enemies import create_enemy, enemy_definition
 from rg_content.quests import SATANIC_FORCES_ID, register_all_quests
+from rg_content.world_events import WORLD_EVENTS, register_all_world_events
 
 register_all_quests()
+register_all_world_events()
 
-__all__ = ["SATANIC_FORCES_ID", "create_enemy", "enemy_definition", "register_all_quests"]
+__all__ = [
+    "SATANIC_FORCES_ID",
+    "WORLD_EVENTS",
+    "create_enemy",
+    "enemy_definition",
+    "register_all_quests",
+    "register_all_world_events",
+]
