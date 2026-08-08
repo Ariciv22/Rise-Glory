86d2bc47ef6863e46ac4fe872d45ba1a6ab83a3f
Ariciv22 <wiktor2202011@gmail.com>
2026-08-08 10:32:22 +0200

    Podepnij stala pozycje mapy heksowej

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ install_legacy_module_aliases()
 
 from rg_core import app as _app
 from rg_ui.map_background_fix import install_map_background
+from rg_ui.map_camera_lock import install_locked_map_camera
 from rg_ui.menu_button_fix import install_menu_button_fix
 from rg_ui.title_flow import install_into_main
 
@@ -20,6 +21,10 @@ install_menu_button_fix()
 
 # Zamiast czarnego tla pod plansza rysujemy grafike Grafiki/tlo_heksow.png.
 install_map_background()
+
+# Plansza heksowa pozostaje stale wycentrowana w obszarze mapy. Gracz moze
+# tylko zmieniac zoom; przeciaganie i zmiana aktywnego bohatera jej nie przesuwa.
+install_locked_map_camera()
 
 # Zachowujemy zgodnosc z poprawkami ekranu tytulowego, ktore odwolują sie do
 # modulu "main". Faktyczna aplikacja jest teraz w rg_core.app.
