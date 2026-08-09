ff3a3e3c34634a378eddaa9ea5c2e1746d789bfb
Ariciv22 <wiktor2202011@gmail.com>
2026-08-09 19:27:04 +0200

    Resetuj pełny stan Rady po opuszczeniu ekranu

--- rg_ui/council_market_rules_bridge.py
+++ rg_ui/council_market_rules_bridge.py
@@ -7,6 +7,7 @@ from rg_ui.council_market_presenter import draw_council as _draw_presented_counc
 install_council_market_rules()
 
 _ORIGINAL_SET_PREP_CATEGORY = market_ui._set_prep_category
+_ORIGINAL_CONFIRM_DEPARTURE = market_ui._confirm_departure
 
 
 def _tracked_set_prep_category(category):
@@ -18,7 +19,19 @@ def _tracked_set_prep_category(category):
     _ORIGINAL_SET_PREP_CATEGORY(category)
 
 
+def _confirm_departure_and_reset(session, player_index):
+    result = _ORIGINAL_CONFIRM_DEPARTURE(session, player_index)
+    if result == "close_council":
+        from rg_ui import council as legacy_council
+        from rg_ui import council_flow
+
+        legacy_council._SESSION = None
+        council_flow.reset_council_intro_flow()
+    return result
+
+
 market_ui._set_prep_category = _tracked_set_prep_category
+market_ui._confirm_departure = _confirm_departure_and_reset
 
 
 def draw_council(screen, title_font, font, small_font, mouse, round_number):
