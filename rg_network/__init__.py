f9d1038918d697269b7012c3eb3e9c19d6fe00aa
Ariciv22 <wiktor2202011@gmail.com>
2026-08-10 21:15:08 +0200

    Eksportuj stan gry LAN

--- rg_network/__init__.py
+++ rg_network/__init__.py
@@ -1,9 +1,11 @@
 """Warstwa sieciowa Rise & Glory.
 
-Pierwszy etap obsluguje lobby w lokalnej sieci LAN. Kod nie zalezy od Pygame,
-dzieki czemu ten sam transport bedzie mozna pozniej wykorzystac przez Internet.
+Pierwszy etap obsluguje lobby i wspolna rozgrywke w lokalnej sieci LAN.
+Transport oraz autorytatywny stan gry sa oddzielone od Pygame, aby ten sam
+model klient-serwer mozna bylo pozniej wystawic przez Internet.
 """
 
+from rg_network.game_state import NetworkGameSession
 from rg_network.lan_client import LanClient
 from rg_network.lan_server import LanLobbyServer, get_lan_ipv4
 from rg_network.protocol import DEFAULT_PORT, PROTOCOL_VERSION
@@ -13,5 +15,6 @@ __all__ = [
     "PROTOCOL_VERSION",
     "LanClient",
     "LanLobbyServer",
+    "NetworkGameSession",
     "get_lan_ipv4",
 ]
