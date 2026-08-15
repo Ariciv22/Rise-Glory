4eb246d214ff214593f3830fa1fdb3f72444716f
Ariciv22 <wiktor2202011@gmail.com>
2026-08-15 11:16:57 +0200

    Register module 1 threats as game content

--- rg_content/__init__.py
+++ rg_content/__init__.py
@@ -2,16 +2,31 @@
 
 from rg_content.enemies import create_enemy, enemy_definition
 from rg_content.quests import SATANIC_FORCES_ID, register_all_quests
+from rg_content.threats import (
+    CHOROBA_DRZEW_ID,
+    LAWINY_ID,
+    ROZBOJNICY_ID,
+    THREATS,
+    ZAWALONE_PRZEJSCIA_ID,
+    register_all_threats,
+)
 from rg_content.world_events import WORLD_EVENTS, register_all_world_events
 
 register_all_quests()
 register_all_world_events()
+register_all_threats()
 
 __all__ = [
     "SATANIC_FORCES_ID",
     "WORLD_EVENTS",
+    "THREATS",
+    "ROZBOJNICY_ID",
+    "LAWINY_ID",
+    "CHOROBA_DRZEW_ID",
+    "ZAWALONE_PRZEJSCIA_ID",
     "create_enemy",
     "enemy_definition",
     "register_all_quests",
     "register_all_world_events",
+    "register_all_threats",
 ]
