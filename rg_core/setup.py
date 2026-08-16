df57de604899a3e8123f971809abf9f74755877c
Ariciv22 <wiktor2202011@gmail.com>
2026-08-16 15:56:20 +0200

    Podlacz efekty Znacznikow Questow

--- rg_core/setup.py
+++ rg_core/setup.py
@@ -18,6 +18,7 @@ from rg_core.data import HERO_ARCHETYPES, STAT_NAMES, clone_hero
 from rg_engine.heroes import ensure_hero_state
 from rg_engine.quest_chronicle_bridge import install_quest_chronicle_bridge
 from rg_engine.quest_council_bridge import install_quest_council_bridge
+from rg_engine.quest_effect_bridge import install_quest_effect_bridge
 from rg_engine.quest_time_bridge import install_quest_time_bridge
 from rg_engine.quests import reset_quest_deck
 from rg_engine.threats import is_tile_entry_blocked, threat_modifier
@@ -126,6 +127,7 @@ def install_council_trade_background():
 install_adventure_system()
 install_world_event_markers()
 install_quest_markers()
+install_quest_effect_bridge()
 install_quest_time_bridge()
 install_world_state_ui()
 install_threat_investigation_ui()
