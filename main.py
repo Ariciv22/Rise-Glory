bfe01d15823754e799cd2a324bc9d4be529f12e7
Ariciv22 <wiktor2202011@gmail.com>
2026-08-23 08:37:21 +0200

    Install location navigation spacing fix

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ from rg_ui.city_hub import install_location_hub
 from rg_ui.dev_council_reset import install_dev_council_reset
 from rg_ui.hud_top_stat_theme import install_hud_top_stat_theme
 from rg_ui.location_edge_bar_theme import install_location_edge_bar_theme
+from rg_ui.location_navigation_fix import install_location_navigation_fix
 from rg_ui.map_background_fix import install_map_background
 from rg_ui.map_camera_lock import install_locked_map_camera
 from rg_ui.menu_button_fix import install_menu_button_fix
@@ -73,6 +74,11 @@ install_village_hub(_app)
 # a srodkowa scena jest dobierana z numeru oraz typu konkretnej lokacji.
 # Prawy content nadal pozostaje pustym kontenerem do dalszego projektowania.
 install_location_hub(_app)
+
+# Strzalki na dole prawego panelu sa centrowane w prawdziwej wolnej stopce
+# pomiedzy separatorem ostatniego slotu a ozdobna rama dolna. Przy renderze
+# wycinany jest tez pusty czarny margines zapisany dookola assetow strzalek.
+install_location_navigation_fix()
 
 # Gorny i dolny pas ekranu lokacji korzystaja z pelnego panel2.png, dzieki
 # czemu widoczne sa zlote naroza i ornamenty na calej ramie zamiast samych linii.
