5c0c52a2e8bee00d7d04e97bd83f4359c7538650
Ariciv22 <wiktor2202011@gmail.com>
2026-08-28 23:11:29 +0200

    Register final quests 24-30

--- rg_content/quests_final.py
+++ rg_content/quests_final.py
@@ -2,7 +2,7 @@
 
 Ten modul jest jedynym aktywnym rejestrem tresci Questow w grze.
 Stare/testowe definicje pozostaja poza aktywna gra i nie sa tutaj importowane.
-Questy 1-23 sa zaakceptowane projektowo i stanowia aktywna zawartosc produkcyjna.
+Questy 1-30 stanowia aktywna zawartosc produkcyjna.
 """
 
 from __future__ import annotations
@@ -18,17 +18,18 @@ install()
 from rg_content.quests_pack_01_08 import QUESTS_01_08
 from rg_content.quests_pack_09_16 import QUESTS_09_16
 from rg_content.quests_pack_17_23 import QUESTS_17_23
+from rg_content.quests_pack_24_30 import QUESTS_24_30
 from rg_engine.quests import register_quest, register_quest_expansion
 
 
-QUESTS = QUESTS_01_08 + QUESTS_09_16 + QUESTS_17_23
+QUESTS = QUESTS_01_08 + QUESTS_09_16 + QUESTS_17_23 + QUESTS_24_30
 EXPANSIONS = ()
 
 _REGISTERED = False
 
 
 def register_all_quests() -> None:
-    """Rejestruje wylacznie finalne Questy produkcyjne 1-23."""
+    """Rejestruje wylacznie finalne Questy produkcyjne 1-30."""
     global _REGISTERED
     if _REGISTERED:
         return
