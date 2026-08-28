7bef348cfade85dfe40599ddbd1f5e7c60fde38e
Ariciv22 <wiktor2202011@gmail.com>
2026-08-28 22:52:18 +0200

    Activate final quests 1 through 23

--- rg_content/quests_final.py
+++ rg_content/quests_final.py
@@ -2,27 +2,46 @@
 
 Ten modul jest jedynym aktywnym rejestrem tresci Questow w grze.
 Stare/testowe definicje pozostaja poza aktywna gra i nie sa tutaj importowane.
-Kolejne Questy trafiaja tu dopiero po zatwierdzeniu w trello/QUESTY_FINAL.md.
+Questy 1-23 sa zaakceptowane projektowo i stanowia aktywna zawartosc produkcyjna.
 """
 
 from __future__ import annotations
 
+import copy
+
+from rg_content.quest_runtime_ext import install
+
+# Rozszerzenia musza zostac podpiete zanim inne moduly pobiora referencje
+# do funkcji create_offer / draw_quest_id / activate_quest.
+install()
+
+from rg_content.quests_pack_01_08 import QUESTS_01_08
+from rg_content.quests_pack_09_16 import QUESTS_09_16
+from rg_content.quests_pack_17_23 import QUESTS_17_23
 from rg_engine.quests import register_quest, register_quest_expansion
 
 
-QUESTS = ()
+QUESTS = QUESTS_01_08 + QUESTS_09_16 + QUESTS_17_23
 EXPANSIONS = ()
 
 _REGISTERED = False
 
 
 def register_all_quests() -> None:
-    """Rejestruje wyłącznie finalne Questy produkcyjne."""
+    """Rejestruje wylacznie finalne Questy produkcyjne 1-23."""
     global _REGISTERED
     if _REGISTERED:
         return
+
     for quest in QUESTS:
-        register_quest(quest)
+        # board_location odpowiada za Tablice Ogloszen. required_location na
+        # poziomie calego Questa pozostawiamy puste, bo dalsze etapy moga
+        # prowadzic na trakt, do lasu albo do innej lokacji. Ograniczenie
+        # miejsca jest zapisane bezposrednio na konkretnym etapie.
+        definition = copy.deepcopy(quest)
+        definition["required_location"] = ""
+        register_quest(definition)
+
     for expansion in EXPANSIONS:
         register_quest_expansion(expansion)
     _REGISTERED = True
