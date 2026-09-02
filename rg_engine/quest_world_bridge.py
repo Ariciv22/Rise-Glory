b6164bf57e82c2a63df7513b15317547082a5124
Ariciv22 <wiktor2202011@gmail.com>
2026-09-02 19:32:53 +0200

    Clear tracked quest boards between world generations

--- rg_engine/quest_world_bridge.py
+++ rg_engine/quest_world_bridge.py
@@ -10,6 +10,7 @@ def install_quest_world_bridge() -> None:
 
     import rg_world
     import rg_world.generation as generation
+    from rg_engine.quest_location_bridge import clear_tracked_quest_locations
     from rg_engine.quests import reset_quest_deck
 
     original_generate_world = rg_world.generate_world
@@ -18,9 +19,10 @@ def install_quest_world_bridge() -> None:
     def try_generate_world_with_fresh_quest_deck(map_key, rng):
         # Generator moze odrzucic kilka map zanim znajdzie poprawny uklad
         # jurysdykcji i Zakladow. Kazda taka proba tworzy 9 lokacji i rezerwuje
-        # Questy na ich Tablicach, dlatego talia musi byc czyszczona PRZED
-        # KAZDA proba, a nie tylko raz przed calym generate_world().
+        # Questy na ich Tablicach, dlatego talia ORAZ lista sledzonych Tablic
+        # musza byc czyszczone PRZED KAZDA proba.
         reset_quest_deck()
+        clear_tracked_quest_locations()
         return original_try_generate_world(map_key, rng)
 
     generation._try_generate_world = try_generate_world_with_fresh_quest_deck
@@ -29,6 +31,7 @@ def install_quest_world_bridge() -> None:
         # Pierwszy reset usuwa stan poprzedniej rozgrywki. Kolejne resety sa
         # wykonywane przez wrapper _try_generate_world przy kazdej probie mapy.
         reset_quest_deck()
+        clear_tracked_quest_locations()
         return original_generate_world(map_key)
 
     rg_world.generate_world = generate_world_with_fresh_quest_deck
