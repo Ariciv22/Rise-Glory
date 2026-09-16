c178a992270607d845877c53b738e77e9beae226
Ariciv22 <wiktor2202011@gmail.com>
2026-09-16 12:31:50 +0200

    Aktywuj powrot do menu glownego z ESC

--- rg_ui/player_board_return_button.py
+++ rg_ui/player_board_return_button.py
@@ -33,8 +33,16 @@ def install_player_board_return_button() -> None:
     if _INSTALLED:
         return
 
+    from rg_core import app as app_module
     from rg_ui import player_board
+    from rg_ui.pause_main_menu import install_pause_main_menu
 
     _ORIGINAL_LAYOUT = player_board.player_board_layout
     player_board.player_board_layout = _player_board_layout_with_bottom_return
+
+    # Ten instalator jest uruchamiany z glownego bootstrapu po zaladowaniu app.py,
+    # dlatego w tym samym miejscu bezpiecznie podpinamy menu pauzy z powrotem
+    # do menu glownego. Sam kod pozostaje w osobnym module pause_main_menu.py.
+    install_pause_main_menu(app_module)
+
     _INSTALLED = True
